d2f3894edb9327c480e0a8aeab17a537cfd2c91d
Jake Brukhman <jake@coinfund.io>
2023-04-16 11:22:44 -0400

    ignore file and changes

--- langpal/__main__.py
+++ langpal/__main__.py
@@ -1,6 +1,11 @@
+from langchain.llms import OpenAI
+
 
 def main():
-    print("hello world")
+    llm = OpenAI(temperature=0.9, model_name="gpt-3.5-turbo")
+    while True:
+        text = input('\n> ')
+        print(llm(text))
 
 if __name__ == "__main__":
     main()
